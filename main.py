7a01eb271939dfcf505c75e89d3359faf8714aa3
ironchan02 <ironchan02@gmail.com>
2022-01-29 17:04:00 +0800

    Added createSocketRoom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from flask_socketio import SocketIO
+from flask import Flask, jsonify, request
+from flask_socketio import SocketIO, join_room, leave_room, rooms
 
 N = 100
 
@@ -7,6 +7,7 @@ app = Flask(__name__)
 socket = SocketIO(app)
 room_list = [False] * N
 free_room = 0
+
 
 @app.route("/api/room", methods=["POST"])
 def createRoom():
@@ -25,6 +26,7 @@ def createRoom():
                 return jsonify({"Error": "False", "Room ID": room_id})
         return jsonify({"Error": "True", "Msg": "No Free Room"})
 
+
 @app.route("/api/room/<rid>", methods=["GET"])
 def joinRoom(rid: str):
     if not rid.isdigit():
@@ -34,9 +36,22 @@ def joinRoom(rid: str):
     else:
         return jsonify({"Error": "True", "Msg": "Room ID not initialize"})
 
+
 @app.route("/")
 def index():
     return "Hello"
+
+
+@socket.on("join", namespace="/websocket")
+def createSocketRoom(json_data):
+    json_data = request.json
+    print(request.json)  # Debug only
+    room_id = json_data['Room ID']
+    join_room(room_id + "A")
+    join_room(room_id + "B")
+    print(rooms(request.sid))
+    return jsonify({"Error": "False"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
