cc9973a554cfc06732d3d14077b9dcf8df0500dd
ironchan02 <ironchan02@gmail.com>
2022-01-29 15:57:08 +0800

    Bug Fix

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask
+import random
+
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, rooms
 
 app = Flask(__name__)
+socket = SocketIO(app)
+room_list = [False] * 10000
+free_room = 0
+
+
+@app.route('/api/room', methods=['GET'])
+def createRoom():
+    room_id = 1
+    return jsonify({'Room ID': room_id})
 
 
 @app.route('/')
