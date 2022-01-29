26126c9479dbcf4138c3c58ef3065b211f172ce9
murasakiakari <master@murasakiakari.moe>
2022-01-29 16:04:53 +0800

    Bug Fix

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ from flask_socketio import SocketIO, rooms
 
 app = Flask(__name__)
 socket = SocketIO(app)
-room_list = [False] * 10000
+room_list = [False] * 100
 free_room = 0
 
 
 @app.route('/api/room', methods=['GET'])
 def createRoom():
-    room_id = 1
+    global free_room
+    if not room_list[free_room]:
+        room_id = free_room
+        free_room += 1
+    else:
+        for i in range(free_room % 100, (free_room + 100) % 100):
+            if not room_list[i]:
+                room_id = i
+                free_room = i + 1
+                break
     return jsonify({'Room ID': room_id})
 
 
